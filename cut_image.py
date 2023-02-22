ca350436bb54dd0c1434b25861d6a88015fa764f
KSY <zan05134@naver.com>
2023-02-22 09:12:22 +0000

    3 추가 및 cat 사진 올리기

--- cut_image.py
+++ cut_image.py
@@ -1,35 +1,52 @@
 import argparse
 import os
-#import matplotlib.image as mpimg
 from PIL import Image
 import numpy as np
 import random
 
 parser = argparse.ArgumentParser()
 
+# 이미지 이름을 넣습니다.
 parser.add_argument("--image", default="cat.0", type=str)
 parser.add_argument("--path", default="./sample/", type=str)
-parser.add_argument("--col", default=2, type=int)
-parser.add_argument("--row", default=2, type=int)
+parser.add_argument("--nums", default=2, type=int)
 
 args = parser.parse_args()
-#image = mpimg.imread(args.path + args.image + '.jpg')
 image = Image.open(args.path + args.image + '.jpg')
 image = np.array(image)
 
 _row, _col, _ = image.shape
 
-if _row % 2 == 1:
-    image = image[:-1,:,:]
-if _col % 2 == 1:
-    image = image[:,:-1,:]
+if args.nums == 2:
+    if _row % 2 == 1:
+        image = image[:-1,:,:]
+    if _col % 2 == 1:
+        image = image[:,:-1,:]
 
-_row = _row // 2
-_col = _col // 2
+elif args.nums == 3:
+    if _row % 3 == 1:
+        image = image[:-1,:,:]
+    elif _row % 3 == 2:
+        image = image[:-2,:,:]
+    if _col % 3 == 1:
+        image = image[:,:-1,:]
+    elif _col % 3 == 2:
+        image = image[:,:-2,:]
 
-images = [image[:_row,:_col,:], image[:_row,_col:,:], image[_row:,:_col,:], image[_row:,_col:,:]]
+_row = _row // args.nums
+_col = _col // args.nums
+
+if args.nums == 2:
+    images = [image[:_row,:_col,:], image[:_row,_col:,:], image[_row:,:_col,:], image[_row:,_col:,:]]
+
+elif args.nums == 3:
+    images = [image[:_row,:_col,:], image[:_row,_col:_col*2,:], image[:_row,_col*2:,:], 
+    image[_row:_row*2,:_col,:], image[:_row:_row*2,_col:_col*2,:], image[_row:_row*2,_col*2:,:],
+    image[_row*2:,:_col,:], image[_row*2:,_col:_col*2,:], image[_row*2:,_col*2:,:]]
+
 random.shuffle(images)
-for i in range(4): 
+
+for i in range(args.nums * args.nums): 
     if np.random.rand() >= 0.5:
         images[i] = images[i][:,::-1,:]
     if np.random.rand() >= 0.5:
@@ -38,6 +55,3 @@ for i in range(4):
         images[i] = np.rot90(images[i], 1, axes = (0, 1))
     
     np.save(f'./save/{args.image}_{i}.npy', images[i])
-
-    # save_image = Image.fromarray(images[i])
-    # save_image.save(f'./save/{args.image}_{i}.jpg','JPEG')
--- merge_image.py
+++ merge_image.py
@@ -3,17 +3,17 @@ import os
 from PIL import Image
 import numpy as np
 import sys
+from itertools import permutations
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--image", default="cat.0", type=str)
 parser.add_argument("--path", default="./sample/", type=str)
-parser.add_argument("--col", default=2, type=int)
-parser.add_argument("--row", default=2, type=int)
+parser.add_argument("--nums", default=2, type=int)
 
 args = parser.parse_args()
 
-num = args.col * args.row
+num = args.nums * args.nums
 images = []
 
 for i in range(num):
@@ -41,37 +41,96 @@ def _transform(image, i):
     elif i == 3:
         return image[::-1,::-1,:]
 
-def get_measure(image_0, image_1, image_2, image_3):
-    _sum = 0
-    _sum += np.sum(np.abs(image_0[:,-1,:].astype('i') - image_1[:,0,:].astype('i')))
-    _sum += np.sum(np.abs(image_0[-1].astype('i') - image_2[0].astype('i')))
-    _sum += np.sum(np.abs(image_1[-1].astype('i') - image_3[0].astype('i')))
-    _sum += np.sum(np.abs(image_2[:,-1,:].astype('i') - image_3[:,0,:].astype('i')))
-
-    return _sum
-
 _min = sys.maxsize
 new_images = []
 
-idxs = [(1,2,3), (1,3,2), (2,1,3), (2,3,1), (3,1,2), (3,2,1)]
+images = [i.astype('i') for i in images]
 
-for idx in idxs:
-    for x0 in range(4):
-        image_0 = _transform(images[0],x0)
-        for x1 in range(4):
-            image_1 = _transform(images[idx[0]],x1)
-            for x2 in range(4):
-                image_2 = _transform(images[idx[1]],x2)
-                for x3 in range(4):
-                    image_3 = _transform(images[idx[2]],x3)
-                    _sum = get_measure(image_0, image_1, image_2, image_3)
-                    if _min > _sum:
-                        _min = _sum
-                        new_images = [image_0, image_1, image_2, image_3]
+if args.nums == 2:
 
-tem1 = np.concatenate((new_images[0],new_images[1]),axis=1)
-tem2 = np.concatenate((new_images[2],new_images[3]),axis=1)
-new_image = np.concatenate((tem1, tem2),axis=0)
+    def get_measure(image_0, image_1, image_2, image_3):
+        _sum = 0
+        _sum += np.sum(np.abs(image_0[:,-1,:] - image_1[:,0,:]))
+        _sum += np.sum(np.abs(image_0[-1] - image_2[0]))
+        _sum += np.sum(np.abs(image_1[-1] - image_3[0]))
+        _sum += np.sum(np.abs(image_2[:,-1,:] - image_3[:,0,:]))
+
+        return _sum
+
+    idxs = [i for i in permutations([1,2,3], 3)]
+
+    for idx in idxs:
+        for x0 in range(4):
+            image_0 = _transform(images[0],x0)
+            for x1 in range(4):
+                image_1 = _transform(images[idx[0]],x1)
+                for x2 in range(4):
+                    image_2 = _transform(images[idx[1]],x2)
+                    for x3 in range(4):
+                        image_3 = _transform(images[idx[2]],x3)
+                        _sum = get_measure(image_0, image_1, image_2, image_3)
+                        if _min > _sum:
+                            _min = _sum
+                            new_images = [image_0, image_1, image_2, image_3]
+
+    tem1 = np.concatenate((new_images[0],new_images[1]),axis=1)
+    tem2 = np.concatenate((new_images[2],new_images[3]),axis=1)
+    new_image = np.concatenate((tem1, tem2),axis=0)
+
+
+elif args.nums == 3:
+
+    def get_measure(image_0, image_1, image_2, image_3, image_4, image_5, image_6, image_7, image_8):
+        _sum = 0
+        _sum += np.sum(np.abs(image_0[:,-1,:] - image_1[:,0,:]))
+        _sum += np.sum(np.abs(image_1[:,-1,:] - image_2[:,0,:]))
+        _sum += np.sum(np.abs(image_3[:,-1,:] - image_4[:,0,:]))
+        _sum += np.sum(np.abs(image_4[:,-1,:] - image_5[:,0,:]))
+        _sum += np.sum(np.abs(image_6[:,-1,:] - image_7[:,0,:]))
+        _sum += np.sum(np.abs(image_7[:,-1,:] - image_8[:,0,:]))
+
+        _sum += np.sum(np.abs(image_0[-1] - image_3[0]))
+        _sum += np.sum(np.abs(image_1[-1] - image_4[0]))
+        _sum += np.sum(np.abs(image_2[-1] - image_5[0]))
+        _sum += np.sum(np.abs(image_3[-1] - image_6[0]))
+        _sum += np.sum(np.abs(image_4[-1] - image_7[0]))
+        _sum += np.sum(np.abs(image_5[-1] - image_8[0]))
+
+        return _sum
+    
+    idxs = [i for i in permutations([1,2,3,4,5,6,7,8], 8)]
+    print(len(idxs))
+    cnt = 0
+    for idx in idxs:
+        for x0 in range(4):
+            image_0 = _transform(images[0],x0)
+            for x1 in range(4):
+                image_1 = _transform(images[idx[0]],x1)
+                for x2 in range(4):
+                    image_2 = _transform(images[idx[1]],x2)
+                    for x3 in range(4):
+                        image_3 = _transform(images[idx[2]],x3)
+                        for x4 in range(4):
+                            image_4 = _transform(images[idx[3]],x4)
+                            for x5 in range(4):
+                                image_5 = _transform(images[idx[4]],x5)
+                                for x6 in range(4):
+                                    image_6 = _transform(images[idx[5]],x6)
+                                    for x7 in range(4):
+                                        image_7 = _transform(images[idx[6]],x7)
+                                        for x8 in range(4):
+                                            image_8 = _transform(images[idx[7]],x8)
+                                            _sum = get_measure(image_0, image_1, image_2, image_3, image_4, image_5, image_6, image_7, image_8)
+                                            if _min > _sum:
+                                                _min = _sum
+                                                new_images = [image_0, image_1, image_2, image_3, image_4, image_5, image_6, image_7, image_8]
+        cnt += 1
+        print(cnt)
+
+    tem1 = np.concatenate((new_images[0],new_images[1],new_images[2]),axis=1)
+    tem2 = np.concatenate((new_images[3],new_images[4],new_images[5]),axis=1)
+    tem2 = np.concatenate((new_images[6],new_images[7],new_images[8]),axis=1)
+    new_image = np.concatenate((tem1, tem2, tem3),axis=0)
 
 real_image = Image.open(args.path + args.image + '.jpg')
 real_image = np.array(real_image)
@@ -79,8 +138,8 @@ real_image = np.array(real_image)
 def img_check(real_image, new_image):
     if real_image.shape[0] == new_image.shape[0]:
         for i in range(4):
-            if np.min(np.abs(real_image.astype('i') - _transform(new_image, i).astype('i'))) == 0:
-                save_image = Image.fromarray(_transform(new_image, i))
+            if np.min(np.abs(real_image - _transform(new_image, i))) == 0:
+                save_image = Image.fromarray(_transform(new_image.astype(np.uint8), i))
                 save_image.save(f'./save/{args.image}_success.jpg','JPEG')
                 return True
 
@@ -91,8 +150,8 @@ def img_check(real_image, new_image):
     else:
         new_image = np.rot90(new_image, 1, axes = (0, 1))
         for i in range(4):
-            if np.max(np.abs(real_image.astype('i') - _transform(new_image, i).astype('i'))) == 0:
-                save_image = Image.fromarray(_transform(new_image, i))
+            if np.max(np.abs(real_image - _transform(new_image, i))) == 0:
+                save_image = Image.fromarray(_transform(new_image.astype(np.uint8), i))
                 save_image.save(f'./save/{args.image}_success.jpg','JPEG')
                 return True
 
@@ -101,9 +160,21 @@ def img_check(real_image, new_image):
         return False
 
 _row_real, _col_real, _ = real_image.shape
-if _row_real % 2 == 1:
-    real_image = real_image[:-1,:,:]
-if _col_real % 2 == 1:
-    real_image = real_image[:,:-1,:]
+
+if args.nums == 2:
+    if _row_real % 2 == 1:
+        image = image[:-1,:,:]
+    if _col_real % 2 == 1:
+        image = image[:,:-1,:]
+
+elif args.nums == 3:
+    if _row_real % 3 == 1:
+        image = image[:-1,:,:]
+    elif _row_real % 3 == 2:
+        image = image[:-2,:,:]
+    if _col_real % 3 == 1:
+        image = image[:,:-1,:]
+    elif _col_real % 3 == 2:
+        image = image[:,:-2,:]
 
 print(img_check(real_image, new_image))
